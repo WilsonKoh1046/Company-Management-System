e6c298e79d0a153234fe8a81f8d3ab2313acb433
Wilson Koh <NikoKoh98@gmail.com>
2020-05-10 01:10:23 +0800

    basic frontend: create new employee entry

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function submit() {
+    let name = document.getElementById("employeeName").value;
+    let role = document.getElementById("employeeRole").value;
+    let salary = document.getElementById("employeeSalary").value;
+
+    let employeeData = {"employeeName": name, "employeeRole": role, "employeeSalary": salary};
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/employee/add",
+        data: JSON.stringify(employeeData),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    alert("Successfully created new employee!");
+}
